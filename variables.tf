a8829163e9c5a47e51ddc0a9452bae6378db4ad8
SESA444562 <sesa444562@se.com>
2024-06-07 12:31:02 +0200

    app insights variables are mandatory

--- variables.tf
+++ variables.tf
@@ -29,12 +29,10 @@ variable "application_insights_connection_string" {
   description = "Connection string for Application Insights"
   type        = string
   sensitive   = true
-  default     = ""
 }
 
 variable "application_insights_key" {
   description = "Application Insights Key"
   type        = string
   sensitive   = true
-  default     = ""
 }
