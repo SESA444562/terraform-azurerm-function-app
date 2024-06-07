25730577a26340aacd28c2c95dc2777bd86548c5
SESA444562 <sesa444562@se.com>
2024-06-07 12:35:42 +0200

    fix data access

--- main.tf
+++ main.tf
@@ -5,16 +5,16 @@ data "azurerm_resource_group" "existing" {
 
 resource "azurerm_service_plan" "example" {
   name                = "example-app-service-plan"
-  resource_group_name = azurerm_resource_group.existing.name
-  location            = azurerm_resource_group.existing.location
+  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
   os_type             = "Linux"
   sku_name            = "Y1"
 }
 
 resource "azurerm_linux_function_app" "example" {
   name                = "example-linux-function-app"
-  resource_group_name = azurerm_resource_group.existing.name
-  location            = azurerm_resource_group.existing.location
+  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
 
   service_plan_id = azurerm_service_plan.example.id
 
