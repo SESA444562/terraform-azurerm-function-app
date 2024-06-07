5b59381bc95ae289a33de759ac5e89e91d1876f1
SESA444562 <sesa444562@se.com>
2024-06-07 11:37:54 +0200

    Add waypoint variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,24 @@
+variable "waypoint_application" {
+  type        = string
+  description = "Name of the Waypoint application."
+
+  validation {
+    condition     = !contains(["-", "_"], var.waypoint_application)
+    error_message = "waypoint_application must not contain dashes or underscores."
+  }
+}
+
+variable "waypoint_add_on_definition" {
+  type        = string
+  description = "Name of the Waypoint add-on definition."
+}
+
+variable "waypoint_add_on_instance" {
+  type        = string
+  description = "Name of the Waypoint add-on instance."
+
+}
+
 variable "resource_group_name" {
   type        = string
   description = "Name of the resource group"
